172441e01f9635d62fd280b7d0b4f689f12f1470
YarotxD <theresistance1234567@gmail.com>
2025-11-30 13:21:29 -0400

    update api url for production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://unpropitiously-unspiritualised-ferne.ngrok-free.dev/api'
+  apiBaseUrl: 'https://web-production-645f3.up.railway.app/api'
 };
